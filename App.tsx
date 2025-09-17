a899d9e0e2ea9916d58de575d5a2d087a98daf36
jorgenbuilder <jorgen@buildnode.io>
2025-09-17 07:30:24 -0700

    a little more demo

--- App.tsx
+++ App.tsx
@@ -1,17 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { Canvas } from "@react-three/fiber";
-import { MeshPortalMaterial } from "@react-three/drei";
+import { MeshPortalMaterial, OrbitControls } from "@react-three/drei";
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <Canvas style={{ flex: 1 }}>
+        <OrbitControls />
         <mesh>
-          <planeGeometry />
+          <planeGeometry args={[3, 5]} />
           <MeshPortalMaterial resolution={1024} blur={0} samples={0}>
             <mesh>
+              <planeGeometry args={[3, 5]} />
+              <meshBasicMaterial color="wheat" />
+            </mesh>
+            <mesh>
               <sphereGeometry />
+              <meshBasicMaterial color="salmon" />
+            </mesh>
+            <mesh position={[-2.25, 0, 0]}>
+              <sphereGeometry />
+              <meshBasicMaterial color="salmon" />
+            </mesh>
+            <mesh position={[2.25, 0, 0]}>
+              <sphereGeometry />
+              <meshBasicMaterial color="salmon" />
             </mesh>
           </MeshPortalMaterial>
         </mesh>
